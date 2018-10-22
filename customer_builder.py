c57bc5ee80ce193731328aa378cf67cc7173c3d3
phil-krull <pkrull@codingdojo.com>
2018-10-22 10:49:56 -0500

    initalizing commit history

---
+++ customer_builder.py
@@ -0,0 +1,10 @@
+class Customer:
+    def __init__(self, name, email, address, phone_number, password):
+        self.name = name
+        self.email = email
+        self.address = address
+        self.phone_number = phone_number
+        self.password = password
+
+
+
