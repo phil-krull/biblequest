eed803046de3f5f94546b0ea38bfeddd6dee0c29
root <root@ip-172-31-15-58.us-east-2.compute.internal>
2019-04-08 17:33:48 +0000

    changed and overwritting mail settings

--- biblequest/__init__.py
+++ biblequest/__init__.py
@@ -10,7 +10,7 @@ app.config.update(dict(
     MAIL_USE_TLS = True,
     MAIL_USE_SSL = False,
     MAIL_USERNAME = 'pkrull@codingdojo.com',
-    MAIL_PASSWORD = 'BowHunter24',
+    MAIL_PASSWORD = '@NeverGiveUp24',
 ))
 # set up directory for file upload
 UPLOAD_FILES = os.path.abspath(os.path.dirname(__file__)) + '/config/uploaded_files/'
